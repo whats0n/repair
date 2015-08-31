3fbd90a791d4c13baf720f0b77a2b0af0b461bc9
whats0n <mrdvv@mail.ru>
2015-08-31 12:45:48 +0300

    upd

---
+++ js/common.js
@@ -0,0 +1,70 @@
+head.ready(function() {
+
+	// $(document).on("click", function(){
+	// 	$(".js-popup").hide();
+	// });
+
+	// function scrollFixedElements() {
+	//     var scroll_left = $(this).scrollLeft();
+	//     $(".fixed-element").css({
+	//         left: -scroll_left
+	//     });
+	// }
+	// scrollFixedElements();
+	// $(window).scroll(function(){
+	//     scrollFixedElements()
+	// });
+
+	// console.log($('body').html());
+	$('.js-calendar').hide();
+
+	$('.js-calendar-btn').click(function(){	    
+		$('.js-calendar').toggle();
+	});
+
+	$('.js-calendar').datepicker({
+		onSelect: function(value, date) { 
+			$(this).parents('.js-calendar-parent').find('.js-calendar-label').text(value)
+			$('.js-calendar').hide(); 
+		} 
+	});
+
+	$(document).click(function() {
+		$(".js-select").removeClass("is-active");
+		  $(".js-select-list").slideUp(100);
+	});
+	
+	// select list
+	$("body").on("click",".js-select",function(event) {
+		event.stopPropagation();
+	});
+	$("body").on("click",".js-select-text",function(event) {
+		var select = $(this).parents(".js-select");
+		if (select.hasClass("is-active")) {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+		}
+		else {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+			select.toggleClass("is-active").find(".js-select-list").slideToggle(100);
+		}
+	   
+	});
+
+	$("body").on("click",".js-select-list li",function() {
+		var val = $(this).attr("data-val");
+		var text = $(this).text();
+		var select = $(this).parents(".js-select");
+		var selectList = $(this).parents(".js-select-list");
+		select.find(".js-select-text").text(text);
+		select.find("option").removeAttr("selected");
+		select.find('option[value="'+val+'"]').attr("selected", "selected");
+		selectList.find("li").removeClass("is-active");
+		$(this).addClass("is-active");
+		select.removeClass("is-active");
+		selectList.slideUp(100);
+		return false;
+		
+	});
+});
---
+++ js/init.js
@@ -0,0 +1,4 @@
+head.load("js/lib/jquery.js", 
+					"js/lib/cycle.js",
+					"js/lib/jquery-ui.min.js",
+					"js/common.js");
