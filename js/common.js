1c0cbfe257469566f595c0d04db08c2dc1062994
whats0n <mrdvv@mail.ru>
2015-09-03 22:14:58 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -29,41 +29,50 @@ head.ready(function() {
 		} 
 	});
 
+	$('.js-datepicker-due').datepicker({
+		beforeShow: function() { 
+			setTimeout(function() {
+				$('.ui-datepicker').addClass('ui-datepicker_mod');
+				$('.ui-datepicker').append('<div class="ui-datepicker__bottom"><label><span>Time</span><input class="ui-datepicker__input"></label><button class="button ui-datepicker-btn">Done</button></div>');
+			}, 50);
+		}
+	});
+
 	//select
 
 	//multi start
 
-	$('.js-multi-select').each(function() {
-		var select = $(this),
-			placeholder = select.attr('placeholder');
+	// $('.js-multi-select').each(function() {
+	// 	var select = $(this),
+	// 		placeholder = select.attr('placeholder');
 
-		select.multipleSelect({
-			single: false,
-			placeholder: placeholder,
-			onOpen: function() {
-				if (select.hasClass('is-active')) {
-					select.removeClass('is-active');
-				}
-				else {
-					select.addClass('is-active')
-				}
-				$(this).toggleClass('is-active');
-				console.log($(this));
+	// 	select.multipleSelect({
+	// 		single: false,
+	// 		placeholder: placeholder,
+	// 		onOpen: function() {
+	// 			if (select.hasClass('is-active')) {
+	// 				select.removeClass('is-active');
+	// 			}
+	// 			else {
+	// 				select.addClass('is-active')
+	// 			}
+	// 			$(this).toggleClass('is-active');
+	// 			console.log($(this));
 				
-			},
-			onClose: function() {
-				if (select.hasClass('is-active')) {
-					select.removeClass('is-active');
-				}
-				else {
-					select.addClass('is-active')
-				}
-				$(this).toggleClass('is-active');
-				console.log($(this));
+	// 		},
+	// 		onClose: function() {
+	// 			if (select.hasClass('is-active')) {
+	// 				select.removeClass('is-active');
+	// 			}
+	// 			else {
+	// 				select.addClass('is-active')
+	// 			}
+	// 			$(this).toggleClass('is-active');
+	// 			console.log($(this));
 				
-			}
-		});
-	});
+	// 		}
+	// 	});
+	// });
 
 	//multi end
 
@@ -113,6 +122,7 @@ head.ready(function() {
 	});
 	$('.js-accordion-link').click(function() {
 		$(this).toggleClass('is-active');
+		$(this).parents('.js-accordion').find('.js-accordion-arr').toggleClass('is-active');
 		$(this).parents('.js-accordion').find('.js-accordion-block').slideToggle('fast');
 		return false;
 	});
@@ -126,6 +136,64 @@ head.ready(function() {
 
 	//chosen
 	$(".js-select-tags").select2({
-	  tags: true
-	})
+	  tags: true,
+	});
+
+
+	$('.js-select-search').each(function() {
+
+		var place = $(this).attr('placeholder');
+
+		$(this).select2({
+			placeholder: place,
+		}).on('select2:open', function() {
+			$('.select2-search__field').attr('placeholder', 'Search');
+		});
+	});
+
+
+	$(document).ready(function() {
+		$(document).click(function() {
+			$(".js-status-list").hide();
+			$(".js-status").removeClass("is-active");
+		});
+		function selectList() {
+			var status = $(".js-status");
+			var status_list = $(".js-status-list");
+			$("body").on("click", ".js-status", function(event){
+				if ($(this).hasClass("is-active")) {
+					status.removeClass("is-active");
+					status_list.slideUp(200);
+				}
+				else {
+					status.removeClass("is-active");
+					status_list.slideUp(200);
+					$(this).find(".js-status-list").slideDown(200);
+					$(this).addClass("is-active");
+				}
+				event.stopPropagation();
+			});
+			$("body").on("click", ".js-status-list li", function(event){
+				var id = $(this).attr("data-id");
+				var text = $(this).text();
+				var selectList = $(this).parents(".js-status-list");
+				selectList.find("li").removeClass("is-active");
+				$(this).addClass("is-active");
+				$(this).parents(".js-status").find(".js-status-text").html($(this).html());
+				$(this).parents(".js-status").find(".js-status-input").val(id);
+				$(this).parent().slideUp(200);
+				$(this).parents(".js-status").removeClass("is-active");
+				event.stopPropagation();
+			});
+		}  
+		
+		selectList();
+		$("body").on("click", ".js-status", function(event){
+			event.stopPropagation();
+		});
+		
+	});
+	
+
+	
 });
--- js/init.js
+++ js/init.js
@@ -3,4 +3,5 @@ head.load("js/lib/jquery.js",
 					"js/lib/jquery-ui.min.js",
 					// "js/lib/jquery.multiple.select.js",
 					"js/lib/select2.full.min.js",
+					"js/lib/chosen.jquery.min.js",
 					"js/common.js");
